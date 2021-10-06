66918ccbab0235495ac1b9bf655db644e7aa855d
Jaro <petrovskyjaroslav@gmail.com>
2021-10-06 16:35:11 +0200

    Rastliny zaznamy

---
+++ rastliny/rastliny_zaznamy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RastlinyZaznamyConfig(AppConfig):
+    name = 'rastliny_zaznamy'
---
+++ rastliny/rastliny_zaznamy/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    kategoria = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    count = models.IntegerField()
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+
+
+    def __str__(self):
+        return self.title
